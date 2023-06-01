4076e1cba1e5407d88727d4e5724016e0e779aab
sudsymoss <james1mowat@gmail.com>
2023-06-01 19:00:27 +1200

    mobile site redirect

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,17 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { register } from 'next-offline/runtime';
 import { useEffect, useState } from 'react';
+import UAParser from 'ua-parser-js';
+import { useRouter } from 'next/router';
 export default function App({ Component, pageProps }) {
+  const parser = new UAParser();
+  const userAgent = parser.getDevice()
+  const router = useRouter()
   useEffect(()=>{
     //register('https://beta.jamesmowat.com/service-worker.js')
+    if(userAgent.type === 'mobile'){
+      router.push('https://m.noti.jamesmowat.com/')
+    }
     register(`${process.env.NEXT_PUBLIC_CURRENTURL}/service-worker.js`)
   })
   return (
