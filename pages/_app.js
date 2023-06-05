7d6b85b19f4aa194a86908f4383f867994396f7b
sudsymoss <james1mowat@gmail.com>
2023-06-05 14:42:52 +1200

    dark toasts

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
   })
   return (
     <>
-      <ToastContainer position="top-left" />
+      <ToastContainer position="top-left" theme='dark' />
       <Component {...pageProps} />
     </>
   )
