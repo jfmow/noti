d94bcbab44035a8719820a10d334f525963a6eee
sudsymoss <james1mowat@gmail.com>
2023-06-08 12:07:25 +1200

    docs embed

--- components/Editor.js
+++ components/Editor.js
@@ -208,6 +208,14 @@ function Editor({ page, preview }) {
                                     width: 600,
                                     id: (groups) => groups.join('/')
                                 },
+                                customDocs: {
+                                    regex: /https?:\/\/notidb\.suddsy\.dev\/api\/files\/videos\/([^\/\?\&]*)\/([^\/\?\&]*)\.docx/,
+                                    embedUrl: 'https://docs.google.com/viewerng/viewer?url=<%= remote_id %>.docx',
+                                    html: "<iframe  width='100%' height='500' style='border: none;'></iframe>",
+                                    height: 300,
+                                    width: 600,
+                                    id: (groups) => groups.join('/')
+                                },
                             },
                         },
                     },
