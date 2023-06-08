fedee1dd2e95694f7d72566bc5a17922bb03a883
sudsymoss <james1mowat@gmail.com>
2023-06-08 12:20:18 +1200

    embed

--- components/Editor.js
+++ components/Editor.js
@@ -210,7 +210,7 @@ function Editor({ page, preview }) {
                                 },
                                 customDocs: {
                                     regex: /https?:\/\/notidb\.suddsy\.dev\/api\/files\/videos\/([^\/\?\&]*)\/([^\/\?\&]*)\.docx/,
-                                    embedUrl: 'https://docs.google.com/viewerng/viewer?url=<%= remote_id %>.docx',
+                                    embedUrl: 'https://docs.google.com/viewerng/viewer?url=<%= remote_id %>.docx&embedded=true',
                                     html: "<iframe  width='100%' height='500' style='border: none;'></iframe>",
                                     height: 300,
                                     width: 600,
