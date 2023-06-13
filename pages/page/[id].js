77ec35e85ddf45eaa55fafdc95dcf3e40ed2e007
sudsymoss <james1mowat@gmail.com>
2023-06-14 09:10:30 +1200

    ping

--- pages/page/[id].js
+++ pages/page/[id].js
@@ -10,6 +10,48 @@ pb.autoCancellation(false);
 const Editor = dynamic(() => import('../../components/Editor'), {
   ssr: false,
 });
+
+async function ping(){
+  async function getCurrentDateTime(timeZone) {
+    const oneHourAgo = new Date();
+
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      timeZone: timeZone
+    };
+
+    const formatter = new Intl.DateTimeFormat('en-US', options);
+    const parts = formatter.formatToParts(oneHourAgo);
+    const year = parts.find(part => part.type === 'year').value;
+    const month = parts.find(part => part.type === 'month').value.padStart(2, '0');
+    const day = parts.find(part => part.type === 'day').value.padStart(2, '0');
+    const hours = parts.find(part => part.type === 'hour').value.padStart(2, '0');
+    const minutes = parts.find(part => part.type === 'minute').value.padStart(2, '0');
+    const seconds = parts.find(part => part.type === 'second').value.padStart(2, '0');
+
+    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return timestamp;
+  }
+
+
+
+
+
+  const date = await getCurrentDateTime(pb.authStore.model.time_zone);
+  const data = {
+    "last_active": date
+  };
+
+  const record = await pb.collection('users').update(pb.authStore.model.id, data);
+}
+
 function NotionEditor({ pageId }) {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -29,51 +71,13 @@ function NotionEditor({ pageId }) {
 
     }
     authUpdate()
-    console.log('gi')
+    ping()
     const lastActiveInti = setInterval(async () => {
-      async function getCurrentDateTime(timeZone) {
-        const oneHourAgo = new Date();
-
-        const options = {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-          hour12: false,
-          timeZone: timeZone
-        };
-
-        const formatter = new Intl.DateTimeFormat('en-US', options);
-        const parts = formatter.formatToParts(oneHourAgo);
-        const year = parts.find(part => part.type === 'year').value;
-        const month = parts.find(part => part.type === 'month').value.padStart(2, '0');
-        const day = parts.find(part => part.type === 'day').value.padStart(2, '0');
-        const hours = parts.find(part => part.type === 'hour').value.padStart(2, '0');
-        const minutes = parts.find(part => part.type === 'minute').value.padStart(2, '0');
-        const seconds = parts.find(part => part.type === 'second').value.padStart(2, '0');
-
-        const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-        return timestamp;
-      }
-
-
-
-
-
-      const date = await getCurrentDateTime(pb.authStore.model.time_zone);
-      const data = {
-        "last_active": date
-      };
-
-      const record = await pb.collection('users').update(pb.authStore.model.id, data);
+      ping()
       //console.log(record)
     }, 450000);
     return () => {
       clearInterval(lastActiveInti);
-      console.log('ff')
     };
   }, [])
   if (isLoading) {
