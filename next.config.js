9a79804d99ce7692ffca9f7bf123d5b292a65c13
sudsymoss <james1mowat@gmail.com>
2023-06-14 10:12:05 +1200

    csp

--- next.config.js
+++ next.config.js
@@ -3,7 +3,24 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['news1.suddsy.dev', 'news2.suddsy.dev', '127.0.0.1', 'notidb.suddsy.dev'],
-  }
+  },
+  async headers() {
+    if (process.env.PRODUCTION === 'true') {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self' noti.jamesmowat.com; style-src 'self' 'unsafe-inline'; frame-src 'self' docs.google.com notidb.suddsy.dev; connect-src 'self' notidb.suddsy.dev; img-src 'self' notidb.suddsy.dev;",
+            },
+          ],
+        },
+      ];
+    }
+    // Return an empty array if not in production
+    return [];
+  },
 }
 
 const withOffline = require('next-offline');
