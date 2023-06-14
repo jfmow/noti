d8b65ea3c988ec2a956deb195e71b2ca18ed79c1
sudsymoss <james1mowat@gmail.com>
2023-06-14 12:26:29 +1200

    acc

--- pages/u/me/index.js
+++ pages/u/me/index.js
@@ -367,6 +367,8 @@ function AccManagementForm() {
               <div className={styles.default_settings_modal_container} onClick={() => { setNotiField(false) }}>
                 <div className={styles.default_settings_modal_container_usrname_bg}>
                   <div className={styles.default_settings_modal_container_usrname_block} onClick={(event) => event.stopPropagation()}>
+                  <button type='button' onClick={()=>setNotiField(false)} className={styles.default_settings_modal_container_usrclose_btn}><svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M480 618 270 828q-9 9-21 9t-21-9q-9-9-9-21t9-21l210-210-210-210q-9-9-9-21t9-21q9-9 21-9t21 9l210 210 210-210q9-9 21-9t21 9q9 9 9 21t-9 21L522 576l210 210q9 9 9 21t-9 21q-9 9-21 9t-21-9L480 618Z" /></svg></button>
+
                     <form>
                       <h2>Toggle notifications</h2>
 
@@ -449,7 +451,9 @@ function Notitoggle() {
 
     } catch (err) {
       console.log(err)
-      return toast.error('Permision denied. Enable notifs')
+      setPush(false)
+      setPendingPush(false)
+      return toast.error('Permision denied!')
     }
 
   }
