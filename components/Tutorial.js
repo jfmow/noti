2ee2515962098b4fefb4398ea2e44d897c2ceb12
sudsymoss <james1mowat@gmail.com>
2023-06-25 17:04:44 +1200

    access

--- components/Tutorial.js
+++ components/Tutorial.js
@@ -6,7 +6,7 @@ export default function Tut({ setHidden }) {
         { name: "Welcome to Noti", info: "Click 'Next' to start the tutorial. (Instructions are for desktop users. Mobile users may have a slightly different layout, but it's essentially the same.)" },
         { name: "Creating a page", info: "To create a page, click '+ Create Page'. To create a subpage within a page, select the desired page and click the '+' icon on the right." },
         { name: "Managing pages", info: "All the options you need are to manage the page are the small white buttons on the right. Click them to see their functions. Do not click the trash can icon, as it permanently deletes the page and cannot this can not undone." },
-        { name: "Setting a title", info: "Click on the title text to set a new title, but don't forget to click the little tick on the right of it to save it!" },
+        { name: "Setting a title", info: "1) Click on the title text. 2) Make your changes. 3) Save by clicking the small tick icon on the right." },
     ];
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
     useEffect(() => {
