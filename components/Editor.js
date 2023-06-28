8c84621d48e9d6e7c11509992795ec1ab7020c10
sudsymoss <james1mowat@gmail.com>
2023-06-28 12:32:34 +1200

    delete items

--- components/Editor.js
+++ components/Editor.js
@@ -859,12 +859,11 @@ class SimpleIframe {
     };
   }
 }
-
 class SimpleTodo {
   static get toolbox() {
     return {
       title: "Checkbox list",
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><rect fill="none" height="24" width="24"/><g><path d="M22,8c0-0.55-0.45-1-1-1h-7c-0.55,0-1,0.45-1,1s0.45,1,1,1h7C21.55,9,22,8.55,22,8z M13,16c0,0.55,0.45,1,1,1h7 c0.55,0,1-0.45,1-1c0-0.55-0.45-1-1-1h-7C13.45,15,13,15.45,13,16z M10.47,4.63c0.39,0.39,0.39,1.02,0,1.41l-4.23,4.25 c-0.39,0.39-1.02,0.39-1.42,0L2.7,8.16c-0.39-0.39-0.39-1.02,0-1.41c0.39-0.39,1.02-0.39,1.41,0l1.42,1.42l3.54-3.54 C9.45,4.25,10.09,4.25,10.47,4.63z M10.48,12.64c0.39,0.39,0.39,1.02,0,1.41l-4.23,4.25c-0.39,0.39-1.02,0.39-1.42,0L2.7,16.16 c-0.39-0.39-0.39-1.02,0-1.41s1.02-0.39,1.41,0l1.42,1.42l3.54-3.54C9.45,12.25,10.09,12.25,10.48,12.64L10.48,12.64z"/></g></svg>',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"><rect fill="none" height="24" width="24"/><g><path d="M22,8c0-0.55-0.45-1-1-1h-7c-0.55,0-1,0.45-1,1s0.45,1,1,1h7C21.55,9,22,8.55,22,8z M13,16c0,0.55,0.45,1,1,1h7 c0.55,0,1-0.45,1-1c0-0.55-0.45-1-1-1h-7C13.45,15,13,15.45,13,16z M10.47,4.63c0.39,0.39,0.39,1.02,0,1.41l-4.23,4.25 c-0.39,0.39-1.02,0.39-1.42,0L2.7,8.16c-0.39-0.39-0.39-1.02,0-1.41c0.39-0.39,1.02-0.39,1.41,0l1.42,1.42l3.54-3.54 C9.45,4.25,10.09,4.25,10.47,4.63z M10.48,12.64c0.39,0.39,0.39,1.02,0,1.41l-4.23,4.25c-0.39,0.39-1.02,0.39-1.42,0L2.7,16.16 c-0.39-0.39-0.39-1.02,0-1.41s1.02-0.39,1.41,0l1.42,1.42l3.54-3.54C9.45,12.25,10.09,12.25,10.48,12.64L10.48,12.64z"/></g></svg>',
     };
   }
 
@@ -883,7 +882,7 @@ class SimpleTodo {
     list.classList.add(styles.todolist);
 
     if (this.data && this.data.items) {
-      this.data.items.forEach((item) => {
+      this.data.items.forEach((item, index) => {
         const listItem = document.createElement("li");
 
         const label = document.createElement("label");
@@ -903,11 +902,34 @@ class SimpleTodo {
 
         const content = document.createElement("span");
         content.textContent = item.content;
+        content.contentEditable = true; // Make the content editable
+
+        content.addEventListener("input", () => {
+          item.content = content.textContent;
+        });
+
         listItem.appendChild(content);
 
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add(styles.tododeletebtn)
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => {
+          this.data.items.splice(index, 1);
+          listItem.remove();
+        });
         checkbox.addEventListener("change", () => {
           item.checked = checkbox.checked;
+          if (item.checked) {
+
+            listItem.appendChild(deleteButton);
+          } else {
+            listItem.removeChild(deleteButton);
+          }
         });
+        if (item.checked) {
+          listItem.appendChild(deleteButton);
+        }
+
 
         list.appendChild(listItem);
       });
@@ -918,7 +940,7 @@ class SimpleTodo {
     const addItemInput = document.createElement("input");
     addItemInput.type = "text";
     addItemInput.placeholder = "+ Add an item";
-    addItemInput.classList.add(styles.todoiteminput)
+    addItemInput.classList.add(styles.todoiteminput);
     addItemInput.addEventListener("keydown", (event) => {
       if (event.key === "Enter" && addItemInput.value.trim() !== "") {
         const newItem = {
@@ -945,13 +967,22 @@ class SimpleTodo {
 
         const content = document.createElement("span");
         content.textContent = newItem.content;
+        content.contentEditable = true; // Make the content editable
+
+        content.addEventListener("input", () => {
+          newItem.content = content.textContent;
+        });
+
         listItem.appendChild(content);
+
+        
 
         checkbox.addEventListener("change", () => {
           newItem.checked = checkbox.checked;
         });
 
         list.appendChild(listItem);
+        
 
         addItemInput.value = "";
       }
@@ -963,10 +994,12 @@ class SimpleTodo {
     return this.wrapper;
   }
 
+
   save() {
     return this.data;
   }
 }
+
 
 
 
