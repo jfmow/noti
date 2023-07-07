a484c0ee1f711dde08b048ebeb4af35eee81ffa9
sudsymoss <james1mowat@gmail.com>
2023-07-07 16:37:20 +1200

    icons filter

--- components/Icons.js
+++ components/Icons.js
@@ -6,18 +6,29 @@ import { AlternateButton, ModalContainer, ModalForm, ModalInput, ModalTitle } fr
 export default function Icons({ Select, Selected, Close }) {
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredEmojis, setFilteredEmojis] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     useEffect(() => {
         emojis.sort((a, b) => a.sort_order - b.sort_order);
-      }, []);
+        const uniqueCategories = [...new Set(emojis.map((emoji) => emoji.category))];
+        setCategories(uniqueCategories);
+    }, []);
 
     useEffect(() => {
         const filtered = emojis.filter((emoji) => {
             const shortName = emoji.short_name.toLowerCase();
-            return shortName.includes(searchTerm.toLowerCase());
+            return (
+                shortName.includes(searchTerm.toLowerCase()) &&
+                (!selectedCategory || emoji.category === selectedCategory)
+            );
         });
         setFilteredEmojis(filtered);
-    }, [searchTerm]);
+    }, [searchTerm, selectedCategory]);
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
 
     function setNewIcon(e) {
         Select(e);
@@ -27,7 +38,25 @@ export default function Icons({ Select, Selected, Close }) {
         <ModalContainer events={Close}>
             <ModalForm>
                 <ModalTitle>Icons</ModalTitle>
-                <ModalInput chngevent={setSearchTerm} place={"Search by short name..."}></ModalInput>
+                <ModalInput chngevent={setSearchTerm} place={"Search by name..."}></ModalInput>
+                <div className={styles.categories}>
+                    <button
+                        type="button"
+                        className={`${styles.categoryButton}`}
+                        onClick={() => handleCategoryClick('')}
+                    >All</button>
+                    {categories.map((category) => (
+                        <button
+                            key={category}
+                            type="button"
+                            className={`${styles.categoryButton} ${selectedCategory === category ? styles.active : ''
+                                }`}
+                            onClick={() => handleCategoryClick(category)}
+                        >
+                            {category}
+                        </button>
+                    ))}
+                </div>
                 <div className={styles.emojigrid}>
                     {filteredEmojis.map((emoji) => {
                         try {
