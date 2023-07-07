77d326273446fa00808ee7709dd886670f3eb7c4
sudsymoss <james1mowat@gmail.com>
2023-07-07 13:18:34 +1200

    search

--- components/Icons.js
+++ components/Icons.js
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
 import emojis from 'emoji-datasource';
 import styles from '@/styles/emojis.module.css';
-import { AlternateButton, ModalContainer, ModalForm, ModalTitle } from "@/lib/Modal";
+import { AlternateButton, ModalContainer, ModalForm, ModalInput, ModalTitle } from "@/lib/Modal";
 
 export default function Icons({ Select, Selected, Close }) {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredEmojis, setFilteredEmojis] = useState([]);
+
     useEffect(() => {
         emojis.sort((a, b) => a.category.localeCompare(b.category));
     }, []);
+
+    useEffect(() => {
+        const filtered = emojis.filter((emoji) => {
+            const shortName = emoji.short_name.toLowerCase();
+            return shortName.includes(searchTerm.toLowerCase());
+        });
+        setFilteredEmojis(filtered);
+    }, [searchTerm]);
 
     function setNewIcon(e) {
         Select(e);
@@ -16,8 +27,9 @@ export default function Icons({ Select, Selected, Close }) {
         <ModalContainer events={Close}>
             <ModalForm>
                 <ModalTitle>Icons</ModalTitle>
+                <ModalInput chngevent={setSearchTerm} place={"Search by short name..."}></ModalInput>
                 <div className={styles.emojigrid}>
-                    {emojis.map((emoji) => {
+                    {filteredEmojis.map((emoji) => {
                         try {
                             return (
                                 <button
