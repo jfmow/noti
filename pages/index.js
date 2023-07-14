3ca5a6beb98f87746b8465badd6301081d483e7b
sudsymoss <james1mowat@gmail.com>
2023-07-14 14:47:13 +1200

    home page

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,13 @@ pb.autoCancellation(false);
 export default function Home() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
+  const [mobile, setMobile] = useState(false);
 
   useEffect(() => {
+
+    if (window.innerWidth < 800) {
+      setMobile(true)
+    }
     async function authUpdate() {
       try {
         if (!pb.authStore.isValid) {
@@ -54,7 +59,7 @@ export default function Home() {
               Join now
             </Link>
           </div>
-          
+
         </header>
         <div className={styles.sections}>
           <AnimatePresence>
@@ -67,7 +72,13 @@ export default function Home() {
                 <h2>Enjoy new features and updates every week to keep your notes looking fresh.</h2>
                 <p>We only add what we think is necessary. Less junk = less clutter = more productivity = better you! <br />It's a win win</p>
               </div>
-              <img alt="Icons item selector" loading='lazy' className={styles.bg} style={{ boxShadow: 'none', height: '70%' }} src="/static/icons.png" />
+              {mobile ? (
+                <img alt="Icons item selector" id='iconsimg' loading='lazy' className={styles.bg} style={{ boxShadow: 'none', height: '70%' }} src="/static/icons.png" />
+
+              ) : (
+                <img alt="Icons item selector" id='iconsimg' loading='lazy' className={styles.bg} style={{ boxShadow: 'none', height: '70%' }} src="/static/icons-desktop.png" />
+
+              )}
 
             </AnimatedSection>
             <AnimatedSection className={styles.section2}>
@@ -93,16 +104,18 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <img alt="Icons item selector" loading='lazy' className={styles.bg} style={{ boxShadow: 'none', height: '70%' }} src="/static/fast.png" />
+              {!mobile && (
+                <img alt="Icons item selector" loading='lazy' className={styles.bg} style={{ boxShadow: 'none', height: '70%' }} src="/static/fast.png" />
+              )}
             </AnimatedSection>
             {/* Animate the third section */}
             <AnimatedSection className={styles.section4}>
-              <img alt="floating notebook" className={styles.bg} loading='lazy' src="/static/notebookfloating1.jpg" />
+              <img alt="floating notebook" className={styles.bg} loading='lazy' src="/static/features.png" />
               <div>
                 <h2>Just the right amount of features</h2>
                 <div className={styles.fcards}>
                   <div className={styles.fcard}>
-                    <span><div className={styles.fcard_icon}><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"><g><rect fill="none" height="24" width="24"/></g><g><path d="M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10C22,6.48,17.52,2,12,2z M19.46,9.12l-2.78,1.15 c-0.51-1.36-1.58-2.44-2.95-2.94l1.15-2.78C16.98,5.35,18.65,7.02,19.46,9.12z M12,15c-1.66,0-3-1.34-3-3s1.34-3,3-3s3,1.34,3,3 S13.66,15,12,15z M9.13,4.54l1.17,2.78c-1.38,0.5-2.47,1.59-2.98,2.97L4.54,9.13C5.35,7.02,7.02,5.35,9.13,4.54z M4.54,14.87 l2.78-1.15c0.51,1.38,1.59,2.46,2.97,2.96l-1.17,2.78C7.02,18.65,5.35,16.98,4.54,14.87z M14.88,19.46l-1.15-2.78 c1.37-0.51,2.45-1.59,2.95-2.97l2.78,1.17C18.65,16.98,16.98,18.65,14.88,19.46z"/></g></svg>
+                    <span><div className={styles.fcard_icon}><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"><g><rect fill="none" height="24" width="24" /></g><g><path d="M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10C22,6.48,17.52,2,12,2z M19.46,9.12l-2.78,1.15 c-0.51-1.36-1.58-2.44-2.95-2.94l1.15-2.78C16.98,5.35,18.65,7.02,19.46,9.12z M12,15c-1.66,0-3-1.34-3-3s1.34-3,3-3s3,1.34,3,3 S13.66,15,12,15z M9.13,4.54l1.17,2.78c-1.38,0.5-2.47,1.59-2.98,2.97L4.54,9.13C5.35,7.02,7.02,5.35,9.13,4.54z M4.54,14.87 l2.78-1.15c0.51,1.38,1.59,2.46,2.97,2.96l-1.17,2.78C7.02,18.65,5.35,16.98,4.54,14.87z M14.88,19.46l-1.15-2.78 c1.37-0.51,2.45-1.59,2.95-2.97l2.78,1.17C18.65,16.98,16.98,18.65,14.88,19.46z" /></g></svg>
                     </div>Robust</span>
                     <p>With Noti, you can rely on a robust and stable note-taking solution. Our platform offers seamless auto-saving functionality, ensuring that your work is continuously saved as you go. You can focus on your tasks without worrying about losing any important information or accidentally closing your work. Experience a reliable and hassle-free note-taking experience with Noti.</p>
                   </div>
