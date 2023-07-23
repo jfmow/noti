b1f6b8501c74eb330afaae089e327251e5c769b9
sudsymoss <james1mowat@gmail.com>
2023-07-23 16:30:08 +1200

    editor update

--- components/Editor.js
+++ components/Editor.js
@@ -11,7 +11,6 @@ import AttachesTool from "@editorjs/attaches";
 import PocketBase from "pocketbase";
 import styles from "@/styles/Create.module.css";
 import Loader from "./Loader";
-import Embed from "@editorjs/embed";
 import { AES, enc } from "crypto-js";
 import compressImage from "@/lib/CompressImg";
 import dynamic from 'next/dynamic';
@@ -19,6 +18,8 @@ import Router from "next/router";
 import { AnimatePresence } from "framer-motion";
 import { AlternateButton, ModalTempLoader } from "@/lib/Modal";
 import { createRandomMeshGradients } from "@/lib/randomMeshGradient";
+import NestedList from '@editorjs/nested-list';
+import Marker from '@editorjs/marker'
 
 const Icons = dynamic(() => import("./Icons"), {
   loading: () => <ModalTempLoader />,
@@ -317,7 +318,21 @@ function Editor({ page, preview, multi }) {
 
             }
           },
-          
+          SimpleIframeWebpage: {
+            class: SimpleIframeWebpage,
+            
+          },
+          nestedList: {
+            class: NestedList,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: 'unordered'
+            },
+          },
+          Marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M',
+          },
           SimpleTodo: {
             class: SimpleTodo,
             config: {
@@ -329,35 +344,6 @@ function Editor({ page, preview, multi }) {
                 }
               },
             }
-          },
-          embed: {
-            class: Embed,
-            config: {
-              services: {
-                youtube: true,
-                codepen: true,
-                customPdf: {
-                  regex:
-                    /https?:\/\/notidb\.suddsy\.dev\/api\/files\/files\/([^\/\?\&]*)\/([^\/\?\&]*)\.pdf/,
-                  embedUrl:
-                    "https://notidb.suddsy.dev/api/files/files/<%= remote_id %>.pdf",
-                  html: "<iframe  width='100%' height='500' style='border: none;'></iframe>",
-                  height: 300,
-                  width: 600,
-                  id: (groups) => groups.join("/"),
-                },
-                customDocs: {
-                  regex:
-                    /https?:\/\/notidb\.suddsy\.dev\/api\/files\/files\/([^\/\?\&]*)\/([^\/\?\&]*)\.docx/,
-                  embedUrl:
-                    "https://docs.google.com/viewerng/viewer?url=https://notidb.suddsy.dev/api/files/files/<%= remote_id %>.docx&embedded=true",
-                  html: "<iframe  width='100%' height='500' style='border: none;'></iframe>",
-                  height: 300,
-                  width: 600,
-                  id: (groups) => groups.join("/"),
-                },
-              },
-            },
           },
           list: {
             class: List,
@@ -453,7 +439,7 @@ function Editor({ page, preview, multi }) {
               }
 
             },
-            
+
           },
           table: Table,
         },
@@ -849,7 +835,7 @@ class SimpleIframe {
     uploadBtn.textContent = "Upload File";
     uploadBtn.classList.add("cdx-button");
     uploadBtn.style.width = '100%'; // Add a class for styling the button
-    
+
     try {
       fileInput.click(); //open immediately
     } catch (err) {
@@ -1188,7 +1174,7 @@ class Image {
     this.wrapper.appendChild(iframe);
   }
 
-  
+
 
   save(blockContent) {
     try {
@@ -1243,4 +1229,100 @@ function ImportantNote({ classname, importt, page }) {
       </label>
     </div>
   )
+}
+
+
+
+
+
+class SimpleIframeWebpage {
+  static get toolbox() {
+    return {
+      title: "Embed webage",
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px"><g><rect style="stroke: none;" fill="none" height="24" width="24"/><rect style="stroke: none;" fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><path style="stroke: none;" d="M20,4H4C2.9,4,2.01,4.9,2.01,6L2,18c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V6C22,4.9,21.1,4,20,4z M4,9h10.5v3.5H4V9z M4,14.5 h10.5V18L4,18V14.5z M20,18l-3.5,0V9H20V18z"/></g></svg>',
+    };
+  }
+
+  constructor({ data, config }) {
+    this.data = data;
+    this.wrapper = undefined;
+    this.config = config || {};
+  }
+
+  render() {
+    this.wrapper = document.createElement("div");
+    const inputContainer = document.createElement('div')
+
+    inputContainer.style.display = 'grid';
+    inputContainer.style.gridTemplateColumns = '4fr 1fr';
+    inputContainer.style.gap = '10px';
+    inputContainer.style.padding = '10px 0px 10px 0'
+
+    if (this.data && this.data.src) {
+      //const originalUrl = decodeURIComponent(this.data.url).replace(
+      //  /&amp;/g,
+      //  "&"
+      //);
+      this._createIframe(this.data.src);
+      return this.wrapper;
+    }
+
+
+    const fileInput = document.createElement("span");
+    fileInput.contentEditable = true;
+    fileInput.classList.add('cdx-input')
+    fileInput.style.width = '100%';
+
+    const uploadBtn = document.createElement("button");
+    uploadBtn.textContent = "Save link";
+    uploadBtn.classList.add("cdx-button");
+    uploadBtn.style.width = '100%'; // Add a class for styling the button
+
+    try {
+      fileInput.click(); //open immediately
+    } catch (err) {
+      console.warn(err)
+    }
+    uploadBtn.addEventListener("click", () => this._createIframe(fileInput.innerText));
+
+    this.wrapper.appendChild(inputContainer);
+    inputContainer.appendChild(fileInput);
+    inputContainer.appendChild(uploadBtn)
+
+    return this.wrapper;
+  }
+
+  async _createIframe(src) {
+    const iframe = document.createElement("iframe");
+    iframe.classList.add(styles.embedIframe);
+    iframe.style.width = "100%";
+    iframe.style.height = "70vh";
+    iframe.style.border = "2px solid #c29fff";
+    iframe.style.borderRadius = "5px";
+    iframe.src = src;
+
+    this.wrapper.innerHTML = "";
+    this.wrapper.appendChild(iframe);
+    const popOutBTN = document.createElement("button");
+    popOutBTN.classList.add(styles.popOutbtn)
+    popOutBTN.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h5c.55 0 1-.45 1-1s-.45-1-1-1H5c-1.11 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-.55-.45-1-1-1s-1 .45-1 1v5c0 .55-.45 1-1 1zM14 4c0 .55.45 1 1 1h2.59l-9.13 9.13c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L19 6.41V9c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1h-5c-.55 0-1 .45-1 1z"/></svg>`
+    popOutBTN.addEventListener("click", () => {
+      let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=600,left=10,top=10`;
+      open(`${url}`, `SaveMyNotes popup`, params);
+    })
+    this.wrapper.appendChild(popOutBTN)
+  }
+
+  save(blockContent) {
+    try {
+      const iframe = blockContent.querySelector("iframe");
+      const src = iframe.getAttribute('src'); // Retrieve the urlFull attribute
+
+      return {
+        src: src // Include the urlFull in the saved data
+      };
+    } catch (err) {
+      console.log(err)
+    }
+  }
 }
