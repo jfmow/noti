9b8f27773072df4e7dd39feb1e524493a2c93951
sudsymoss <james1mowat@gmail.com>
2023-07-29 11:38:55 +1200

    pdf viewer

--- customEditorTools/SimpleEmbed.js
+++ customEditorTools/SimpleEmbed.js
@@ -93,14 +93,10 @@ export default class SimpleIframe {
       iframe.classList.add(styles.embedIframe);
       iframe.style.width = "100%";
       iframe.style.height = "70vh";
-      iframe.style.border = "2px solid #c29fff";
+      iframe.style.border = "none";
       iframe.style.borderRadius = "5px";
-      const fileToken = await pb.files.getToken();
-      // retrieve an example protected file url (will be valid ~5min)
-  
-      const record = await pb.collection('files').getOne(fileId); // Use the fileId to retrieve the record
-      const url = pb.files.getUrl(record, record.file_data, { 'token': fileToken });
-      iframe.src = url;
+      
+      iframe.src = `${process.env.NEXT_PUBLIC_CURRENTURL}/page/pdf/${fileId}`;
       iframe.setAttribute('fileId', fileId); // Set the fileId as an attribute of the iframe
   
       this.wrapper.innerHTML = "";
@@ -110,7 +106,7 @@ export default class SimpleIframe {
       popOutBTN.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h5c.55 0 1-.45 1-1s-.45-1-1-1H5c-1.11 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-.55-.45-1-1-1s-1 .45-1 1v5c0 .55-.45 1-1 1zM14 4c0 .55.45 1 1 1h2.59l-9.13 9.13c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L19 6.41V9c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1h-5c-.55 0-1 .45-1 1z"/></svg>`
       popOutBTN.addEventListener("click", () => {
         let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=600,left=10,top=10`;
-        open(`${url}`, `SaveMyNotes popup`, params);
+        open(`${process.env.NEXT_PUBLIC_CURRENTURL}/page/pdf/${fileId}`, `SaveMyNotes popup`, params);
       })
       this.wrapper.appendChild(popOutBTN)
     }
---
+++ pages/page/pdf/[id].js
@@ -0,0 +1,39 @@
+import dynamic from 'next/dynamic';
+import Router from 'next/router';
+import PocketBase from "pocketbase";
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL);
+pb.autoCancellation(false)
+const MyPdfViewer = dynamic(() => import('@/customEditorTools/pdfViewer'), {
+  ssr: false // Set ssr: false to prevent server-side rendering and enable client-side rendering for this component
+});
+import { useEffect, useState } from "react";
+
+export default function Viewer({ docId }) {
+  const [url, setUrl] = useState(null)
+  useEffect(() => {
+    if(!pb.authStore.isValid){
+      return Router.push('/auth/login')
+    }
+    async function GetUrl(url3) {
+      const fileToken = await pb.files.getToken();
+      console.log(url3)
+      // retrieve an example protected file url (will be valid ~5min)
+
+      const record = await pb.collection('files').getOne(url3); // Use the fileId to retrieve the record
+      const url2 = pb.files.getUrl(record, record.file_data, { 'token': fileToken });
+      setUrl(url2)
+    }
+    GetUrl(docId)
+  }, [docId])
+  return (
+    <MyPdfViewer url={url} />
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  return {
+    props: {
+      docId: params.id,
+    },
+  };
+}
