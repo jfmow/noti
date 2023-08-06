4b4089359a559c2a408e403dd1fe19e4988c3785
sudsymoss <james1mowat@gmail.com>
2023-08-06 17:37:38 +1200

    url

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel='manifest' href='https://savemynotes.net/manifest.json'/>
+        <link rel='manifest' href={`${process.env.NEXT_PUBLIC_CURRENTURL}/manifest.json`}/>
         </Head>
       <body>
         <Main />
