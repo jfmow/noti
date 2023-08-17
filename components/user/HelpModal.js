f957baaf562d5a6a117ade572ecc5c83468dda42
sudsymoss <james1mowat@gmail.com>
2023-08-17 14:52:45 +1200

    navy

--- components/user/HelpModal.js
+++ components/user/HelpModal.js
@@ -5,17 +5,21 @@ import Router from "next/router";
 export default function UserHelpModal({ CloseHelp }) {
     return (
         <>
-        <Head>
-            <title>Theme</title>
-        </Head>
+            <Head>
+                <title>Theme</title>
+            </Head>
             <ModalContainer events={CloseHelp}>
                 <ModalForm>
                     <ModalTitle>Theme</ModalTitle>
-                    <AlternateButton click={()=>{
+                    <AlternateButton click={() => {
                         window.localStorage.setItem('theme', 'purple')
                         Router.reload()
                     }}>Purple</AlternateButton>
-                    <AlternateButton click={()=>{
+                    <AlternateButton click={() => {
+                        window.localStorage.setItem('theme', 'navy blue')
+                        Router.reload()
+                    }}>Navy blue</AlternateButton>
+                    <AlternateButton click={() => {
                         window.localStorage.setItem('theme', '')
                         Router.reload()
                     }}>System</AlternateButton>
--- pages/_app.js
+++ pages/_app.js
@@ -48,6 +48,40 @@ export default function App({ Component, pageProps }) {
           '--modal_button_bxshdw_hover': '#46425e'
         }
         break;
+      case 'navy blue':
+        vars = {
+          '--background': '#353b44',
+          '--big_button_background': '#353535',
+          '--big_button_border': '#4f4f4f',
+          '--big_button_border_hover_alt': '#2d2d2d',
+          '--big_button_text': '#707684',
+          '--create_text_color': '#fff',
+          '--desktophidemenu_bg': '#464646',
+          '--desktophidemenu_bg_hover': '#040404',
+          '--root_boxshadow': '#1d1d1d',
+          '--itemoption_hover': '#000000',
+          '--itemoption_active': '#0000001a',
+          '--btn1_hover': '#282828',
+          '--itemoption_hover_drag': '#ff7777',
+          '--createpage_txt': '#3d3d3d',
+          '--itemoption_textcolor': '#ffffff99',
+          '--itemoption_fillcolor': '#ffffff99',
+          '--modal_color-23-7309aa07': '#464457',
+          '--modal_color-0-c42e25a8': '#fff',
+          '--modal_color-1-ea5ca4f1': '#414b59',
+          '--modal_color-25-1133eaba': '#373544',
+          '--modal_color-18-c8f98051': '#707684',
+          '--modal_color-26-3e9fea21': '#47435f',
+          '--modal_color-19-8243e20a': '#ff2d2d',
+          '--modal_color-28-8fb448ab': '#d3d3d3',
+          '--modal_color-7-0c0f90f8': '#ebebeb',
+          '--modal_button_bg': '#353b44',
+          '--modal_button_border': 'none',
+          '--modal_button_bg_hover': '#282d34',
+          '--modal_button_text_hover': '#fff',
+          '--modal_button_bxshdw_hover': '#2d333a'
+        }
+        break;
       default:
         break;
     }
