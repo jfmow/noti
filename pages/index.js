c82c3fbb412343f627a11149d2c5f1ab5780a84a
sudsymoss <james1mowat@gmail.com>
2023-09-18 19:03:17 +1200

    home

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,10 @@ export default function Home2() {
         ) : (
           <Nav />
         )}
-        <Section1 />
-        <div>
+
+        <div className={styles.sections}>
+
+          <Section1 />
 
           <div className={`${styles.section}`}>
             <div className={styles.s2_answer_img} >
@@ -48,25 +50,28 @@ export default function Home2() {
           <FeatureCardsSection />
           <CompBanner />
           <BigTextSection1 />
-          <OpenSourseSection />
+          <BuiltByAStudentSection />
+          <BigInspSection />
+
+          <div className={styles.footer}>
+            <h3>Made with <img src='/emoji/twitter/64/1f496.png' width='16' height='16' /> by James M</h3>
+            <h5>
+              Explore on{' '}
+              <Link aria-label="jfmow github link" href="https://github.com/jfmow" target="_blank">
+                <svg className={styles.githublogo} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                  />
+                </svg>
+              </Link>
+            </h5>
+          </div>
 
         </div>
 
-        <div className={styles.footer}>
-          <h3>Made with <img src='/emoji/twitter/64/1f496.png' width='16' height='16' /> by James M</h3>
-          <h5>
-            Explore on{' '}
-            <Link aria-label="jfmow github link" href="https://github.com/jfmow" target="_blank">
-              <svg className={styles.githublogo} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                />
-              </svg>
-            </Link>
-          </h5>
-        </div>
+
       </div>
     </>
   )
@@ -108,7 +113,7 @@ function Nav2() {
     <div className={styles.sticky}>
       <div className={styles.nav}>
         <div className={styles.nav_items_mobile}>
-          <div><img width='200' src='/name.png' /></div>
+          <div><img width='200' src='/name-new.png' /></div>
           <button className={styles.nav_mobile_menu} type='button' onClick={() => setState(!state)}>{state ? (
             <svg id="hide-button" class="h-6 fill-current hidden" viewBox="0 0 20 20"><title>Menu Close</title><polygon points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2" transform="rotate(45 10 10)"></polygon></svg>
           ) : (
@@ -136,7 +141,7 @@ function Nav2() {
 function FeatureCardsSection() {
   return (
     <div className={styles.FeatureCardsSection}>
-      <h2 className={styles.FeatureCardsSection_title}>Everything you could need to create Your notes</h2>
+      <h2 className={styles.FeatureCardsSection_title}>Everything You Could Need To Create Your Notes</h2>
       <div className={styles.FeatureCardsSection_cards}>
         <div className={styles.FeatureCardsSection_card}>
           <div className={styles.FeatureCardsSection_card_title}>
@@ -250,6 +255,49 @@ function OpenSourseSection() {
             Built with open-source tools and packages you know how everything works and know nothing sneaky is going on.
           </p>
         </div>
+      </div>
+    </div>
+  )
+}
+
+function BuiltByAStudentSection() {
+  return (
+    <div className={styles.BuiltByAStudentSection}>
+      <div>
+        <h2 className={styles.BuiltByAStudentSection_title}>Built By A Student</h2>
+        <p className={styles.BuiltByAStudentSection_text}>And created for you!</p>
+      </div>
+      <div className={styles.BuiltByAStudentSection_pics}>
+        <div className={styles.BuiltByAStudentSection_pics_card}>
+          <img className={styles.BuiltByAStudentSection_pics_card_img} src='/bbas1.svg' />
+          <div className={styles.BuiltByAStudentSection_pics_card_text}>
+            <span >Created with simplicty in mind</span>
+          </div>
+        </div>
+        <div className={styles.BuiltByAStudentSection_pics_card}>
+          <img className={styles.BuiltByAStudentSection_pics_card_img} src='/bbas2.svg' />
+          <div className={styles.BuiltByAStudentSection_pics_card_text}>
+            <span >Built with reliablity in mind</span>
+          </div>
+        </div>
+        <div className={styles.BuiltByAStudentSection_pics_card}>
+          <img className={styles.BuiltByAStudentSection_pics_card_img} src='/bbas3.svg' />
+          <div className={styles.BuiltByAStudentSection_pics_card_text}>
+            <span>Used with ideas in mind</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function BigInspSection() {
+  return (
+    <div className={styles.BigInspSection}>
+      <h2 className={styles.BigInspSection_title}>Creativity Comes First!</h2>
+      <div className={styles.BigInspSection_content}>
+        <img className={styles.BigInspSection_img} src='/bis1.svg' />
+        <span className={styles.BigInspSection_text}>We believe that when you let your imagination run wild and soak up knowledge like a sponge, amazing things can happen. Our note-taking app is designed to be your trusty sidekick on your journey of discovery. With colorful highlighting, sweet colors, and loads of other features, you can let your creativity flow freely while taking notes. Plus, our user-friendly interface makes learning a breeze. So, whether you're a student, a writer, or just someone who loves to explore, we've got you covered. Come join us, and let your creativity and learning soar to new heights! 🚀📚🎨</span>
       </div>
     </div>
   )
