80b88841cb7752fab5b0b4042420c5772a2a733b
sudsymoss <james1mowat@gmail.com>
2023-10-08 14:34:06 +1300

    bug

--- components/editor/Menu/MenuButton.js
+++ components/editor/Menu/MenuButton.js
@@ -382,7 +382,6 @@ export default function MenuButtons({ pb, page, editor, clearStates, editorRef, 
                                 //set everything false
                                 setSharePageInfo(false);
                                 setShowPageInfo(false);
-                                setpopUpClickEventPageOptions(false);
                                 setpopUpClickEventMarkdown(null)
                             }} >
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" /><polyline points="16 6 12 2 8 6" /><line x1="12" x2="12" y1="2" y2="15" /></svg>
@@ -392,7 +391,6 @@ export default function MenuButtons({ pb, page, editor, clearStates, editorRef, 
                             //set everything false
                             setSharePageInfo(false);
                             setShowPageInfo(false);
-                            setpopUpClickEventPageOptions(false);
                             setpopUpClickEventMarkdown(null)
                         }}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-from-line"><path d="M3 5v14" /><path d="M21 12H7" /><path d="m15 18 6-6-6-6" /></svg>
@@ -402,7 +400,6 @@ export default function MenuButtons({ pb, page, editor, clearStates, editorRef, 
                             //set everything false
                             setSharePageInfo(false);
                             //setShowPageInfo(true);
-                            setpopUpClickEventPageOptions(false);
                             setpopUpClickEventMarkdown(null)
                         }}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg>
@@ -413,7 +410,6 @@ export default function MenuButtons({ pb, page, editor, clearStates, editorRef, 
                                 //set everything false
                                 setSharePageInfo(false);
                                 setShowPageInfo(false);
-                                setpopUpClickEventPageOptions(false);
                                 setpopUpClickEventMarkdown(null)
                             }}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18" /><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" /><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" /><line x1="10" x2="10" y1="11" y2="17" /><line x1="14" x2="14" y1="11" y2="17" /></svg>
