290562288a5187e88736dd2f9d8245a800ef84da
sudsymoss <james1mowat@gmail.com>
2023-10-12 19:28:56 +1300

    folders and home page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '@/styles/Home.module.css'
 import { useEffect, useState } from 'react'
 import PocketBase from 'pocketbase'
 import Router from 'next/router'
+import { GitHubCat } from '@/lib/Logos'
 const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL)
 export default function Home2() {
   const [windowWidth, setWindowWidth] = useState(false)
@@ -45,13 +46,7 @@ export default function Home2() {
             <h5>
               Explore on{' '}
               <Link aria-label="jfmow github link" href="https://github.com/jfmow" target="_blank">
-                <svg className={styles.githublogo} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                  />
-                </svg>
+                <GitHubCat />
               </Link>
             </h5>
           </div>
@@ -68,9 +63,9 @@ function Section1() {
   return (
     <div className={styles.section1}>
       <div className={styles.center}>
-        <img className={styles.section1_bannerimg} src='/bannera.jpg' />
-        <h1 className={styles.title}>The <span className={styles.section1_title_bg}>Ultimate</span> Note App</h1>
-        <p className={styles.subtitle}>Elevate your <span className={styles.section1_title_bg}>Note</span> taking experience to make your notes better.</p>
+        <img className={styles.section1_bannerimg} src='/8Bit/banner2.jpg' />
+        <h1 className={styles.title}>The <span className={styles.section1_title_bg}>Ultimate</span> Notes App</h1>
+        <p className={styles.subtitle}>Elevate your Note taking experience to make your notes better.</p>
         <Link className={styles.button_s1} href='/auth/signup'>Get Started</Link>
         <span className={styles.button_s1_subtitle}>{`(It's freee)`}</span>
       </div>
@@ -82,7 +77,7 @@ function Nav() {
   return (
     <div className={styles.sticky}>
       <div className={styles.nav}>
-        <div><img width='200' src='/name-new.png' /></div>
+        <div><img width='35' src='/favicon.png' /></div>
         <div className={styles.nav_links}>
           <Link href='/'>Home</Link>
           <Link href='/preview'>Preview</Link>
@@ -102,7 +97,6 @@ function Nav2() {
     <div className={styles.sticky}>
       <div className={styles.nav}>
         <div className={styles.nav_items_mobile}>
-          <div><img width='200' src='/name-new.png' /></div>
           <button className={styles.nav_mobile_menu} type='button' onClick={() => setState(!state)}>{state ? (
             <svg id="hide-button" class="h-6 fill-current hidden" viewBox="0 0 20 20"><title>Menu Close</title><polygon points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2" transform="rotate(45 10 10)"></polygon></svg>
           ) : (
@@ -116,9 +110,6 @@ function Nav2() {
               <Link href='/preview'>Preview</Link>
               <Link href='https://github.com/jfmow/noti'>Selfhost</Link>
               <Link href='/auth/login'>Login</Link>
-            </div>
-            <div className={styles.nav_buttons}>
-              <Link href='/auth/signup' className={styles.nav_button_main}>Get started</Link>
             </div>
           </div>
         )}
@@ -204,8 +195,8 @@ function CompBanner() {
     <div className={styles.CompBanner}>
       <h3>Made by a student, for students, for the community</h3>
       <div className={styles.CompBanner_logos}>
-        <Link href='https://github.com/jfmow/noti'><img src='GitHub_Logo.png' /></Link>
-        <Link href='https://jamesmowat.com'><img src='jamesmowat.svg' /></Link>
+        <Link href='https://github.com/jfmow/noti'><img src='/Brand-assets/GitHub_Logo.png' /></Link>
+        <Link href='https://jamesmowat.com'><img src='/Brand-assets/jamesmowat.svg' /></Link>
       </div>
     </div>
   )
