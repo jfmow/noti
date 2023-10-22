e771d2d44ad878b37f144c1d114759015c72198a
sudsymoss <james1mowat@gmail.com>
2023-10-22 14:29:02 +1300

    logout

--- components/user/AccountButtons2.js
+++ components/user/AccountButtons2.js
@@ -192,6 +192,13 @@ export default function AccountButtons({ event, setpopUpClickEventSettingsModal 
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-x-2"><path d="M14 19a6 6 0 0 0-12 0" /><circle cx="8" cy="9" r="4" /><line x1="17" x2="22" y1="8" y2="13" /><line x1="22" x2="17" y1="8" y2="13" /></svg>
                         Delete account
                     </PopUpCardDropMenuSectionItem>
+                    <PopUpCardDropMenuSectionItem onClick={(e) => {
+                        pb.authStore.clear()
+                        Router.push('/')
+                    }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
+                        Logout
+                    </PopUpCardDropMenuSectionItem>
                 </PopUpCardDropMenuSection>
             </PopUpCardDropMenuStaticPos>
             <>
