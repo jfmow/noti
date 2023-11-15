e86eab31b74cb91e0c4a4b6b3ee7315e3bb59031
sudsymoss <james1mowat@gmail.com>
2023-11-15 16:20:19 +1300

    home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Link, Paragraph } from '@/components/UX-Components'
 import styles from '@/styles/Home2.module.css'
 import Router from 'next/router'
 import { useEffect } from 'react'
-
+import { motion, useScroll } from 'framer-motion'
 export default function Home() {
   useEffect(() => {
     if (window.localStorage.getItem('pocketbase_auth')) {
@@ -128,7 +128,8 @@ function Header() {
 function Features() {
   return (
     <div className={styles.features}>
-      <div className={styles.card}>
+      <motion.div initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 0.85 }} className={styles.card}>
         <ctitle>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles"><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" /><path d="M5 3v4" /><path d="M19 17v4" /><path d="M3 5h4" /><path d="M17 19h4" /></svg>
           Features
@@ -136,8 +137,9 @@ function Features() {
         <Paragraph>
           Easy to use, easy to keep using.<br />Simplifed so you can focus on writing and not worry about how it looks
         </Paragraph>
-      </div>
-      <div className={styles.card}>
+      </motion.div>
+      <motion.div initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 0.9 }} className={styles.card}>
         <ctitle>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mouse-pointer-click"><path d="m9 9 5 12 1.8-5.2L21 14Z" /><path d="M7.2 2.2 8 5.1" /><path d="m5.1 8-2.9-.8" /><path d="M14 4.1 12 6" /><path d="m6 12-1.9 2" /></svg>
           Intuitive
@@ -146,8 +148,9 @@ function Features() {
           Our app's intuitive design ensures that you can start using it right away. No steep learning curve - just open and start creating your notes effortlessly.
 
         </Paragraph>
-      </div>
-      <div className={styles.card}>
+      </motion.div>
+      <motion.div initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 0.95 }} className={styles.card}>
         <ctitle>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bring-to-front"><rect x="8" y="8" width="8" height="8" rx="2" /><path d="M4 10a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2" /><path d="M14 20a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2" /></svg>
           Organized
@@ -155,8 +158,9 @@ function Features() {
         <Paragraph>
           Keep your notes neatly organized with our app. Create folders, categorize your notes, and search effortlessly (coming soon) to find what you need when you need it.
         </Paragraph>
-      </div>
-      <div className={styles.card}>
+      </motion.div>
+      <motion.div initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 1 }} className={styles.card}>
         <ctitle>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
           Open Source
@@ -164,7 +168,7 @@ function Features() {
         <Paragraph>
           Knowing what the software your using is doing should be visible. No hiding behind a curtan. And this allows for community collaboration which helps make these apps even better.
         </Paragraph>
-      </div>
+      </motion.div>
     </div>
   )
 }
