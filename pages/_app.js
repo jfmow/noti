4d716a724782c0d34a667435f42695d30e7fb2b3
sudsymoss <james1mowat@gmail.com>
2023-11-20 19:13:50 +1300

    anayltics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { Toaster } from 'sonner';
 import Router from 'next/router';
 import '@/components/toast.css'
 import { ToastContainer } from '@/components/toasty';
+import { generateDeviceFingerprint } from '@/lib/Fingerprint';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -20,6 +21,34 @@ export default function App({ Component, pageProps }) {
       }
     }
   }, []);
+
+  useEffect(() => {
+    async function Analytics(button) {
+      try {
+        const id = await generateDeviceFingerprint()
+        const currentPage = Router.asPath
+        const data = {
+          url: currentPage,
+          id: id,
+          action: button ? button : ''
+        };
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_CURRENTURL}/api/you`, {
+          method: 'POST', // Specify the HTTP method
+          headers: {
+            'Content-Type': 'application/json', // Specify the content type as JSON
+          },
+          body: JSON.stringify(data), // Convert the data to a JSON string
+        });
+      } catch {
+        console.error('Analytics error')
+      }
+    }
+    Analytics()
+    window.addEventListener('click', (e) => {
+      Analytics((e.target.tagName + ' ' + e.target.innerText) || e.target.tagName + ' button')
+    })
+  })
 
   return (
     <>
