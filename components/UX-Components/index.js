3ea06fe589901e3ea57998f1554ff2bf5901dcf0
sudsymoss <james1mowat@gmail.com>
2023-12-29 08:10:24 +1300

    style fix

--- components/UX-Components/index.js
+++ components/UX-Components/index.js
@@ -3,9 +3,9 @@ export function Input({ label, ...props }) {
     return (
         <div>
             {label && (
-                <label className="block mb-1 font-[500]" for={id}>{label}</label>
+                <label className="block mb-1 font-[500] text-[14px]" for={id}>{label}</label>
             )}
-            <input className="w-full px-1 py-2 rounded border bg-none text-zinc-800 mb-2 font-[400]" name={id} {...props} />
+            <input className="w-full py-[12px] px-[16px] rounded-lg outline-none border border-slate-100 bg-slate-50 text-zinc-800 text-[14px] mb-2 font-[400]" name={id} {...props} />
         </div>
     )
 }
@@ -38,7 +38,7 @@ export function SubmitButton({ alt, ...props }) {
 
 export function Paragraph({ ...props }) {
     return (
-        <p {...props} className="text-[14px] text-zinc-500 font-[500]">
+        <p {...props} className="text-[14px] text-zinc-500 font-[500] mb-2">
             {props?.children}
         </p>
     )
