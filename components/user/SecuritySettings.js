2418d76d9dd7878f6697a419d3e496b855c88b4e
sudsymoss <james1mowat@gmail.com>
2024-01-25 17:30:01 +1300

    toggle email auth

--- components/user/SecuritySettings.js
+++ components/user/SecuritySettings.js
@@ -4,11 +4,11 @@ import { Input, Paragraph, SubmitButton } from "../UX-Components";
 import { useEditorContext } from "@/pages/page/[...id]";
 import { useEffect, useState } from "react";
 import { toaster } from "@/components/toast";
+import Router from "next/router";
 
 export default function SecuritySettings() {
     const { pb } = useEditorContext()
     const [ssoState, setSSOState] = useState(false)
-    const [newPassword, setNewPassword] = useState({ newPass: '', confNewPass: '', required: false })
     useEffect(() => {
         async function CheckSSOState() {
             try {
@@ -19,24 +19,6 @@ export default function SecuritySettings() {
         }
         CheckSSOState()
     }, [pb])
-    async function ToggleSSO(reqNewPassword) {
-        const loadingToast = await toaster.loading(ssoState ? 'Disabling Email Auth' : 'Enabling Email Auth')
-        if (reqNewPassword && (!newPassword.newPass || !newPassword.confNewPass)) {
-            return toaster.info('Please fill out all displayed fields')
-        }
-        try {
-            const state = await pb.send(reqNewPassword ? `/api/auth/sso/toggle?np=${newPassword.newPass}` : `/api/auth/sso/toggle`, { method: 'POST' })
-            setSSOState(!ssoState)
-            toaster.update(loadingToast, ssoState ? 'Email Auth disabled' : 'Email Auth enabled', "info")
-        } catch (err) {
-            console.log(err.data)
-            if (err.data.message === "You must set a password before disabling SSO.") {
-                setNewPassword({ ...newPassword, required: true })
-            }
-            toaster.update(loadingToast, err.data.message, "error")
-        }
-
-    }
 
     async function deleteAccount(e) {
         e.preventDefault();
@@ -71,45 +53,17 @@ export default function SecuritySettings() {
                         <Modal>
                             {ssoState ? (
                                 <>
-                                    <ModalTrigger>
-                                        <DropDownItem>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-square"><path d="M12.4 2.7c.9-.9 2.5-.9 3.4 0l5.5 5.5c.9.9.9 2.5 0 3.4l-3.7 3.7c-.9.9-2.5.9-3.4 0L8.7 9.8c-.9-.9-.9-2.5 0-3.4Z" /><path d="m14 7 3 3" /><path d="M9.4 10.6 2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4" /></svg>
-                                            Disable Email Auth
-                                        </DropDownItem>
-                                    </ModalTrigger>
-                                    <ModalContent>
-                                        {newPassword.required ? (
-                                            <>
-                                                <form onSubmit={(e) => { e.preventDefault(); ToggleSSO(true); }}>
-                                                    <Input autocomplete='new-password' placeholder='New password' type='password' required onChange={(e) => setNewPassword({ ...newPassword, newPass: e.target.value })} />
-                                                    <Input autocomplete='new-password' placeholder='Confirm new password' type='password' required onChange={(e) => setNewPassword({ ...newPassword, confNewPass: e.target.value })} />
-                                                    <SubmitButton type='submit'>Disable</SubmitButton>
-
-                                                </form>
-                                            </>
-                                        ) : (
-                                            <SubmitButton onClick={() => ToggleSSO()}>Disable</SubmitButton>
-                                        )}
-
-                                    </ModalContent>
+                                    <DropDownItem onClick={() => Router.push("/auth/settings/email-auth?state=1")}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-square"><path d="M12.4 2.7c.9-.9 2.5-.9 3.4 0l5.5 5.5c.9.9.9 2.5 0 3.4l-3.7 3.7c-.9.9-2.5.9-3.4 0L8.7 9.8c-.9-.9-.9-2.5 0-3.4Z" /><path d="m14 7 3 3" /><path d="M9.4 10.6 2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4" /></svg>
+                                        Disable Email Auth
+                                    </DropDownItem>
                                 </>
                             ) : (
                                 <>
-                                    <ModalTrigger>
-                                        <DropDownItem>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-square"><path d="M12.4 2.7c.9-.9 2.5-.9 3.4 0l5.5 5.5c.9.9.9 2.5 0 3.4l-3.7 3.7c-.9.9-2.5.9-3.4 0L8.7 9.8c-.9-.9-.9-2.5 0-3.4Z" /><path d="m14 7 3 3" /><path d="M9.4 10.6 2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4" /></svg>
-                                            Enable SSO
-                                        </DropDownItem>
-                                    </ModalTrigger>
-                                    <ModalContent>
-                                        <h1>✨ Say goodbye to passwords!</h1>
-                                        <Gap>10</Gap>
-                                        <Paragraph>
-                                            🌟 Upgrade your login experience! Enable Single Sign-On (SSO) for password-free access using just your email, with the convenience of magic links or codes. Effortless, secure, and compatible with OAuth providers like GitHub. 🚀
-                                        </Paragraph>
-                                        <Gap>20</Gap>
-                                        <SubmitButton onClick={() => ToggleSSO()}>Enable</SubmitButton>
-                                    </ModalContent>
+                                    <DropDownItem onClick={() => Router.push("/auth/settings/email-auth?state=0")}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-square"><path d="M12.4 2.7c.9-.9 2.5-.9 3.4 0l5.5 5.5c.9.9.9 2.5 0 3.4l-3.7 3.7c-.9.9-2.5.9-3.4 0L8.7 9.8c-.9-.9-.9-2.5 0-3.4Z" /><path d="m14 7 3 3" /><path d="M9.4 10.6 2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4" /></svg>
+                                        Enable Email Auth
+                                    </DropDownItem>
                                 </>
                             )}
                         </Modal>
---
+++ pages/auth/settings/email-auth.js
@@ -0,0 +1,80 @@
+import { Link } from "@/components/UX-Components"
+import { Input, Paragraph, SubmitButton } from "@/components/UX-Components"
+import { toaster } from "@/components/toast"
+import { ArrowLeft } from "lucide-react"
+import Router, { useRouter } from "next/router"
+import PocketBase from 'pocketbase'
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL)
+export default function EmailAuth() {
+    const { query } = useRouter()
+    const disabling = Boolean(+query?.state)
+
+    async function handleDisable(e) {
+        e.preventDefault()
+        if (disabling) {
+            const form = new FormData(e.target)
+            const passwordA = form.get("passwordA").trim()
+            const passwordB = form.get("passwordB").trim()
+            if (passwordA !== passwordB) {
+                return toaster.error("Passwords must match")
+            }
+            if (passwordB.length < 8) {
+                return toaster.error("Password must be at least 8 characters long")
+            }
+            try {
+                const req = await pb.send('/api/auth/sso/toggle', { method: "POST", body: form })
+                pb.authStore.clear()
+                Router.push("/auth/login")
+            } catch (err) {
+                toaster.error(err.message)
+            }
+        } else {
+            try {
+                const req = await pb.send('/api/auth/sso/toggle', { method: "POST" })
+                pb.authStore.clear()
+                Router.push("/auth/login")
+            } catch (err) {
+                toaster.error(err.message)
+            }
+        }
+
+    }
+
+    return (
+        <>
+            <div className="w-full h-[100dvh] bg-zinc-50 text-zinc-800 relative">
+                <div class="absolute top-0 h-full w-full bg-white">
+                    <div class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
+                </div>
+                <div className="relative z-[2] w-full h-[100dvh] flex flex-col items-center justify-center px-5 ">
+                    <Link onClick={() => Router.back()} className="absolute top-7 left-7 cursor-pointer">
+                        <ArrowLeft className="w-6 h-6 text-zinc-600" />
+                    </Link>
+                    <div className="w-full max-w-[400px] flex-col">
+                        <h1 className="mb-2 text-left">{disabling ? "Disable Email Auth" : "Enable Email Auth"}</h1>
+                        <form tabIndex={1} onSubmit={handleDisable}>
+                            {disabling ? (
+                                <>
+                                    <p className="font-semibold text-red-500 mt-6 mb-6 p-4 bg-red-100 rounded-xl">
+                                        Important: If you choose to disable "Email Auth," you will no longer be able to use your email to log in with a code. This security feature enhances account protection by replacing traditional passwords. Feel free to reach out to our support team if you have any questions or need assistance. Thank you for your understanding as we work to maintain a secure environment for your account.
+                                    </p>
+                                    <Input name="passwordA" type="password" placeholder="New password" />
+                                    <Input name="passwordB" type="password" placeholder="Confirm new password" />
+                                    <SubmitButton type="submit">Confirm</SubmitButton>
+                                </>
+                            ) : (
+                                <>
+                                    <p className="font-semibold text-green-500 mt-6 mb-6 bg-green-100 p-4 rounded-xl">
+                                        Enabling "Email Auth" will replace your current password with a secure email-based authentication system. This enhances account security by providing a unique code via email for access. If you have any questions, contact our support team. Thank you for prioritizing the security of your account.
+                                    </p>
+                                    <SubmitButton type="submit">Confirm</SubmitButton>
+                                </>
+                            )}
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
