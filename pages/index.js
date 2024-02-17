2f4005d84ebb2f1132dd865d9ddee65c9ccf002b
sudsymoss <james1mowat@gmail.com>
2024-02-18 12:25:13 +1300

    home page

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,11 @@ export default function HOME() {
       </Head>
       <div className="w-full text-zinc-800">
 
-        <div className="w-full min-h-screen sm:justify-center justify-between flex-col flex items-center relative px-5 pb-0">
+        <div className="w-full min-h-screen  relative ">
 
           <div class="absolute top-0 z-[-2] h-full w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
 
-          <nav className="sm:absolute top-0 p-3 text-zinc-700 items-center grid sm:grid-cols-3 min-h-[60px] h-fit z-[50] w-full ">
+          <nav className="p-3 fixed bg-zinc-50 shadow-lg text-zinc-700 items-center grid sm:grid-cols-3 min-h-[60px] h-fit z-[50] w-full ">
             <div className="w-full h-full hidden items-center sm:flex justify-start ">
               <div className="overflow-hidden w-6 h-6 object-contain">
                 <img alt="Brand logo - mountians with lines" src="/logo-small.webp" />
@@ -46,24 +46,26 @@ export default function HOME() {
           </nav>
 
 
-          <div className="flex flex-col justify-center w-full max-w-[1200px] px-5">
-            <div className="mb-2 flex items-center justify-center w-full">
-              <div className="border inline-flex h-full cursor-pointer justify-center items-center rounded-full bg-white px-3 py-1 text-xs font-medium leading-5 text-zinc-600 backdrop-blur-xl">
-                Explore the demo 🤯 <ArrowRight className="w-4 h-4 ml-2" />
+          <div className="inline-flex justify-center items-center w-full h-[100dvh]">
+            <div className="flex flex-col justify-center items-center w-full max-w-[1200px] px-5">
+              <div className="mb-2 flex items-center justify-center w-full">
+                <div className="border inline-flex h-full cursor-pointer justify-center items-center rounded-full bg-white px-3 py-1 text-xs font-medium leading-5 text-zinc-600 backdrop-blur-xl">
+                  Explore the demo 🤯 <ArrowRight className="w-4 h-4 ml-2" />
+                </div>
               </div>
-            </div>
-            <h1 className="text-center text-3xl font-medium text-gray-900 sm:text-6xl">Accelerate your thoughts, <br />become <span className="bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-400 bg-clip-text text-transparent">more creative</span></h1>
-            <div className="py-6 mt-4 flex items-center justify-center gap-6 flex-row mb-12 w-full">
-              <Link href="/auth/signup" className="px-4 py-2 text-md bg-purple-600 text-zinc-100 rounded-md font-medium hover:bg-purple-700">
-                Get started
-              </Link>
-              <Link href="/page/previewwelcome0" className="px-4 py-2 text-md bg-zinc-100 text-zinc-800 rounded-md font-medium hover:bg-zinc-200">
-                View demo
-              </Link>
-            </div>
+              <h1 className="text-center text-3xl font-medium text-gray-900 sm:text-6xl">Accelerate your thoughts, <br />become <span className="bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-400 bg-clip-text text-transparent">more creative</span></h1>
+              <div className="py-6 mt-4 flex items-center justify-center gap-6 flex-row mb-12 w-full">
+                <Link href="/auth/signup" className="px-4 py-2 text-md bg-purple-600 text-zinc-100 rounded-md font-medium hover:bg-purple-700">
+                  Get started
+                </Link>
+                <Link href="/page/previewwelcome0" className="px-4 py-2 text-md bg-zinc-100 text-zinc-800 rounded-md font-medium hover:bg-zinc-200">
+                  View demo
+                </Link>
+              </div>
 
-            <img alt="mobile page preview" width={500} height={1000} className="md:hidden object-contain" src="/mobile-preview.webp" />
-            <img alt="desktop and mobile together - page preview" loading="eager" width={2189} height={1190} className="hidden md:block" src="/desktop-preview.webp" />
+              <img alt="mobile page preview" width={200} height={423} className="md:hidden object-contain" src="/mobile-preview.webp" />
+              <img alt="desktop and mobile together - page preview" loading="eager" width={2189} height={1190} className="hidden md:block" src="/desktop-preview.webp" />
+            </div>
           </div>
         </div>
         <div className="w-full min-h-screen p-5 pb-10 justify-center flex-col flex items-center relative bg-slate-950">
