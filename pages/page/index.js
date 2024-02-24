c1d9e193160a4f76f13f02abe557c624f43f41c7
sudsymoss <james1mowat@gmail.com>
2024-02-24 14:42:44 +1300

    auth bug fix

--- pages/page/index.js
+++ pages/page/index.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import Router, { useRouter } from "next/router";
-import { Loader2 } from "lucide-react";
+import Router from "next/router";
 import PocketBase from 'pocketbase'
 import Loader from "@/components/Loader";
 const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL);
@@ -9,16 +8,22 @@ export default function redirect() {
 
     useEffect(() => {
         async function GetLatestPage() {
-            const urlParams = new URLSearchParams(window.location.search)
             try {
-                const latestPage = await pb.collection("pages").getFirstListItem("", { sort: "-updated" })
-                Router.push(`/page/${latestPage.id}${urlParams.size >= 1 ? `?${urlParams.toString()}` : ""}`)
-            } catch (err) {
-                if (err.data.code === 404) {
-                    const req = await pb.send("/api/collections/users/account/create-empty-page")
-                    Router.push(`/page/${req.id}`)
+                const urlParams = new URLSearchParams(window.location.search)
+                try {
+                    const latestPage = await pb.collection("pages").getFirstListItem("", { sort: "-updated" })
+                    Router.push(`/page/${latestPage.id}${urlParams.size >= 1 ? `?${urlParams.toString()}` : ""}`)
+                } catch (err) {
+                    if (err.data.code === 404) {
+                        const req = await pb.send("/api/collections/users/account/create-empty-page")
+                        Router.push(`/page/${req.id}`)
+                    }
                 }
+            } catch {
+                Router.push("/auth/login")
             }
+
+
         }
         GetLatestPage()
     }, [])
