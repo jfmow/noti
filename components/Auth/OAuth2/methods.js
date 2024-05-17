b16b9ac33391d691038323a934cee07e753bd4f7
sudsymoss <james1mowat@gmail.com>
2024-05-17 12:01:34 +1200

    fix margins overflow

--- components/Auth/OAuth2/methods.js
+++ components/Auth/OAuth2/methods.js
@@ -20,7 +20,7 @@ export default function OAuth2LoginButtons() {
 
     return (
         <div className="mt-2 grid gap-0 w-full">
-            <button className="m-1 flex w-full min-h-[37px] rounded-lg bg-zinc-300 dark:bg-zinc-700 dark:text-zinc-300 items-center justify-center gap-1 px-3 py-2 font-semibold text-sm text-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-600 transition-all duration-300" onClick={() => OAuthLogin("github")}>
+            <button className="mt-1 mb-1 flex w-full min-h-[37px] rounded-lg bg-zinc-300 dark:bg-zinc-700 dark:text-zinc-300 items-center justify-center gap-1 px-3 py-2 font-semibold text-sm text-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-600 transition-all duration-300" onClick={() => OAuthLogin("github")}>
                 <Github className="w-5 h-5" />
                 Github
             </button>
