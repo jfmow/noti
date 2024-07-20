cd69b16d4676d3bb4b49350f5f8bf16fd8629b94
sudsymoss <james1mowat@gmail.com>
2024-07-20 12:10:19 +1200

    csp

--- next.config.js
+++ next.config.js
@@ -12,10 +12,18 @@ const nextConfig = {
           headers: [
             {
               key: 'Content-Security-Policy',
-              value: "default-src 'self' p.suddsy.dev note.suddsy.dev; script-src-elem unpkg.com 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' 'unsafe-inline' fonts.gstatic.com fonts.google.com fonts.googleapis.com; frame-src *; connect-src 'self' unsplash.com api.unsplash.com images.unsplash.com noti.suddsy.dev proti.suddsy.dev; img-src 'self' noti.suddsy.dev proti.suddsy.dev p.suddsy.dev blob:p.suddsy.dev images.unsplash.com unsplash.com data:; worker-src 'self' unpkg.com https://cdn.jsdelivr.net;"
+              value: `
+                default-src 'self' p.suddsy.dev note.suddsy.dev;
+                script-src-elem unpkg.com 'self' 'unsafe-inline';
+                style-src 'self' 'unsafe-inline';
+                font-src 'self' 'unsafe-inline' fonts.gstatic.com fonts.google.com fonts.googleapis.com;
+                frame-src *;
+                connect-src 'self' unsplash.com api.unsplash.com images.unsplash.com noti.suddsy.dev proti.suddsy.dev;
+                img-src 'self' noti.suddsy.dev proti.suddsy.dev p.suddsy.dev images.unsplash.com unsplash.com data: blob:;
+                worker-src 'self' unpkg.com https://cdn.jsdelivr.net;
+              `.replace(/\s{2,}/g, ' ').trim() // Minimize the string for cleaner CSP
             },
           ],
-
         },
       ];
     }
@@ -24,6 +32,6 @@ const nextConfig = {
   },
 }
 
-//const withOffline = require('next-offline');
+// const withOffline = require('next-offline');
 
 module.exports = nextConfig;
