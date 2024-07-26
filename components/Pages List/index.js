cc64d6485fb6fe75a6885332b79633847ede60a9
sudsymoss <james1mowat@gmail.com>
2024-07-26 14:52:33 +1200

    layout

--- components/Pages List/index.js
+++ components/Pages List/index.js
@@ -52,19 +52,21 @@ export default function UsersPages() {
 
     return (
         <>
-            <div id="hidemewhenprinting" style={defaultWidth === "full" ? { width: "100vw", position: "fixed", top: 0, left: 0, right: 0, bottom: 0, zIndex: 2, height: "100svh", display: visible ? "flex" : "none" } : { width: defaultWidth + "px", height: "100dvh" }} className={`bg-[var(--background)] pb-[70px] relative flex-col overflow-hidden ${defaultWidth === "full" ? "" : (visible ? "animate-slideout" : "animate-slidein")}`}>
+            <div id="hidemewhenprinting" style={defaultWidth === "full" ? { width: "100vw", position: "fixed", top: 0, left: 0, right: 0, bottom: 0, zIndex: 2, height: "100svh", display: visible ? "flex" : "none" } : { width: defaultWidth + "px", height: "100dvh" }} className={`bg-[var(--background)] border-r border-zinc-600/20 pt-[70px] relative flex-col overflow-hidden ${defaultWidth === "full" ? "" : (visible ? "animate-slideout" : "animate-slidein")}`}>
                 {loading ? (
                     <Loader />
                 ) : (
                     <>
-                        <ul className='h-full w-full p-2 overflow-y-scroll'>
+
+                        <UserOptions />
+
+                        <ul className='h-full w-full px-5 py-2 overflow-y-scroll'>
                             {listedPageItems.length >= 1 && sortAndNestObjects(listedPageItems).map((item) => (
                                 <PageListItem data={item} />
                             ))}
                             <CreateNewPageButton setListedPageItems={setListedPageItems} />
                         </ul>
 
-                        <UserOptions />
                     </>
                 )}
             </div>
--- components/user-info/index.js
+++ components/user-info/index.js
@@ -1,27 +1,25 @@
-import { Settings2, UserCircle } from 'lucide-react';
+import { Settings, UserCircle } from 'lucide-react';
 import SettingsPopover from '../Settings';
 import pb from "@/lib/pocketbase"
 export default function UserOptions({ clss }) {
     return (
         <>
-            <div className={`${"absolute bottom-0 z-[2] w-full sm:w-full flex items-center justify-between p-2 h-[70px] text-[var(--userOptionText)] border-t border-[var(--userinfoSectionBordertop)] bg-[var(--background)]"} ${clss}`}>
-                <div className='flex items-center h-full'>
-                    <div className="flex items-center justify-center w-8 h-8 object-contain overflow-hidden rounded-[9999px]">
-                        {pb.authStore.model?.avatar ? (
-                            <img src={`${process.env.NEXT_PUBLIC_POCKETURL}/api/files/users/${pb.authStore.model?.id}/${pb.authStore.model?.avatar}?thumb=100x100`} />
-                        ) : (
-                            <UserCircle />
-                        )}
-                    </div>
-                    <div className='select-none flex flex-col justify-center h-full ml-2'>
-                        <span className='text-sm text-ellipsis overflow-hidden w-[calc(1em_*_13)]'>{pb.authStore.model?.username || 'Example'}</span>
-                        <span className='text-xs  font-light text-ellipsis overflow-hidden w-[calc(1em_*_13)]'>{pb.authStore.model?.email || 'example@youremail.com'}</span>
-                    </div>
+            <div className={`${"absolute top-0 z-[2] w-full sm:w-full grid grid-cols-[1fr_3fr_1fr] items-center justify-items-center justify-between px-4 py-2 h-[70px] text-[var(--userOptionText)] shadow bg-[var(--background)]"} ${clss}`}>
+                <div className="flex items-center justify-center w-8 h-8 object-contain overflow-hidden rounded-[9999px]">
+                    {pb.authStore.model?.avatar ? (
+                        <img src={`${process.env.NEXT_PUBLIC_POCKETURL}/api/files/users/${pb.authStore.model?.id}/${pb.authStore.model?.avatar}?thumb=100x100`} />
+                    ) : (
+                        <UserCircle />
+                    )}
+                </div>
+                <div className='select-none flex flex-col justify-center h-full ml-2 max-w-[156px]'>
+                    <span className='text-sm text-ellipsis overflow-hidden w-[calc(1em_*_13)]'>{pb.authStore.model?.username || 'Example'}</span>
+                    <span className='text-xs  font-light text-ellipsis overflow-hidden w-[calc(1em_*_13)]'>{pb.authStore.model?.email || 'example@youremail.com'}</span>
                 </div>
                 <div className="flex flex-col gap-1 relative">
                     <SettingsPopover>
                         <span className='sr-only'>Account settings</span>
-                        <Settings2 className='w-4 h-4 cursor-pointer' />
+                        <Settings className='w-4 h-4 cursor-pointer' />
                     </SettingsPopover>
                 </div>
             </div>
